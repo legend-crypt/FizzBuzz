fcc8eeb297c16f241b6f29a1554ea5bb0484b80b
Lord <yrnlehjend.gmail.com>
2022-04-28 21:54:09 +0000

    create a function to play with computer

--- odin__projects/rockpaper.js
+++ odin__projects/rockpaper.js
@@ -11,3 +11,31 @@ function computerPlay(){
     }
 }
 console.log(computerPlay())
+
+function playRounds(playerSelection, computerSelection){
+ if(playerSelection =="Rock" && computerSelection == "Paper"){
+     return"Player1 wins"
+ }
+ else if(computerSelection =="Rock" && playerSelection == "Paper"){
+    return"computer wins"
+}
+else if(computerSelection =="Paper" && playerSelection == "Rock"){
+    return "computer wins"
+}
+else if(playerSelection =="Paper" && computerSelection == "Paper"){
+    return "player1 wins"
+}
+else if(computerSelection =="Scissor" && playerSelection == "Paper"){
+    return "computer wins"
+}
+else if(playerSelection =="Scissor" && computerSelection == "Paper"){
+    return "player1 wins"
+}
+else {
+    return "Draw"
+}
+
+}
+playerSelection = "Rock"
+computerSelection = computerPlay()
+console.log(playRounds(playerSelection, computerSelection))
